72e1704c56b6c3fc45b044aa29bd4789e75c6125
abiprakash007@gmail.com <abiprakash007@gmail.com>
2022-10-24 12:08:10 +1100

    files to enable julia vscode to build sys image

---
+++ .vscode/sysimage.jl
@@ -0,0 +1,19 @@
+using JuMP, HiGHS, Gurobi, Cbc
+
+for optimizer in (HiGHS.Optimizer, Gurobi.Optimizer, Cbc.Optimizer)
+    model = Model(optimizer)
+    @variable(model, x >= 0)
+    @variable(model, 0 <= y <= 3)
+    @objective(model, Min, 12x + 20y)
+    @constraint(model, c1, 6x + 8y >= 100)
+    @constraint(model, c2, 7x + 12y >= 120)
+    optimize!(model)
+    open("model.log", "w") do io
+        print(io, solution_summary(model; verbose = true))
+        return
+    end
+end
+
+using Plots
+p = plot(rand(2,2))
+display(p)
