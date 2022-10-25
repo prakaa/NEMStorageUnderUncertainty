f79eb74b0a7454b8f41f70adea425f6292a1257e
abiprakash007@gmail.com <abiprakash007@gmail.com>
2022-10-25 11:33:18 +1100

    python cmd line prompt to get data via nemosis and nemseer

---
+++ data_scripts/NEMSUU_getNEMdata/get_data.py
@@ -0,0 +1,41 @@
+from pathlib import Path
+
+from nemosis import cache_compiler
+from nemseer import download_raw_data
+
+
+def main():
+    analysis_start = "2021/01/01 00:00:00"
+    analysis_end = "2022/01/01 00:00:00"
+
+    nemosis_cache = Path("data/dispatch_price")
+    if not nemosis_cache.exists():
+        nemosis_cache.mkdir(parents=True)
+
+    nemseer_cache = Path("data/forecast_price")
+    if not nemseer_cache.exists():
+        nemseer_cache.mkdir(parents=True)
+
+    cache_compiler(
+        analysis_start,
+        analysis_end,
+        "DISPATCHPRICE",
+        nemosis_cache,
+        fformat="parquet",
+    )
+
+    download_raw_data(
+        "PREDISPATCH",
+        "PRICE",
+        nemseer_cache / "PD",
+        forecasted_start=analysis_start,
+        forecasted_end=analysis_end,
+    )
+
+    download_raw_data(
+        "P5MIN",
+        "REGIONSOLUTION",
+        nemseer_cache / "P5MIN",
+        forecasted_start=analysis_start,
+        forecasted_end=analysis_end,
+    )
