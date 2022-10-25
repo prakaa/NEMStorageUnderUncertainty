57b66ded3eff2940d8e05d51a2cbdb559260a88d
abiprakash007@gmail.com <abiprakash007@gmail.com>
2022-10-25 18:09:22 +1100

    add helper functions for model creation and var initialisation

--- src/model.jl
+++ src/model.jl
@@ -1,2 +1,44 @@
-import JuMP
+using Dates, JuMP
 
+function _initialise_model(
+    optimizer;
+    silent=false,
+    time_limit_sec::Union{Float64, Nothing}=nothing
+)
+
+    model = Model(optimizer)
+    if silent
+        set_silent(model)
+    end
+    if !isnothing(time_limit_sec)
+        set_time_limit_sec(model, time_limit_sec)
+    end
+    return model
+end
+
+function _add_power_vars(
+    model::Model,
+    storage::StorageDevice,
+    t::Vector{DateTime}
+)
+    power_cap = storage.power_cap
+    @variable(model, 0.0 ≤ discharge_mw[t] ≤ power_cap)
+    @variable(model, 0.0 ≤ charge_mw[t] ≤ power_cap)
+end
+
+function _add_soc_var(
+    model::Model,
+    storage::StorageDevice,
+    t::Vector{DateTime}
+)
+    min_soc = storage.soc_min
+    max_soc = storage.soc_max
+    @variable(model, min_soc ≤ soc_mwh[t] ≤ max_soc)
+end
+
+function _add_charge_state_var(
+    model::Model,
+    t::Vector{DateTime}
+)
+    @variable(model, charge_state[t], Bin)
+end
