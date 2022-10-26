97955de3b14a3065684b06eb60a8f5dc2964701a
abiprakash007@gmail.com <abiprakash007@gmail.com>
2022-10-26 14:27:48 +1100

    add standard objective

---
+++ src/model/objectives.jl
@@ -0,0 +1,32 @@
+@doc raw"""
+    _add_objective_standard!(
+    model::JuMP.Model, prices::Vector{AbstractFloat}, times::Vector{DateTime}, τ::Float64
+    )
+
+Adds a standard revenue-maximising objective function:
+
+```math
+\begin{aligned}
+\max_{t} \quad & \sum_{t}{\tau\lambda_t(p_t - q_t)}
+\end{aligned}
+```
+# Arguments
+
+  * `model`: JuMP model
+  * `prices`: A `Vector` of prices in $/MWh
+  * `times`: A `Vector` of `DateTime`s
+  * `τ`: Frequency of `prices` in hours
+
+"""
+function _add_objective_standard!(
+    model::JuMP.Model, prices::Vector{AbstractFloat}, times::Vector{DateTime}, τ::Float64
+)
+    @objective(
+        model,
+        Max,
+        sum(
+            prices[_get_times_index(t, times)] * τ * (discharge_mw[t] - charge_mw[t]) for
+            t in times
+        )
+    )
+end
