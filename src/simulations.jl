e9d1d92a52a9c1a7dec325571aa3b0ed97e164e8
abiprakash007@gmail.com <abiprakash007@gmail.com>
2022-10-26 18:07:35 +1100

    add debug statements and return input data

--- src/simulations.jl
+++ src/simulations.jl
@@ -8,15 +8,19 @@ function _simulate_actual_prices(
     if region ∉ ("QLD1", "NSW1", "VIC1", "SA1", "TAS1")
         throw(ArgumentError("Invalid region"))
     end
+    @debug "Loading all actual prices"
     actual = get_all_actual_prices(actual_price_data_path)
     if !isnothing(time_window)
+        @debug "Filtering actual prices"
         actual = get_prices_by_times(actual, time_window)
     end
     df = actual.data
+    @debug "Filtering by region, then obtaining times and prices"
     filter!(:REGIONID => x -> x == region, df)
     times = df[:, :SETTLEMENTDATE]
     prices = df[:, :RRP]
+    @debug "Building model"
     model = build_storage_model(storage, prices, times, StandardFormulation())
     JuMP.set_optimizer(model, optimizer)
-    return model
+    return (model, df)
 end
