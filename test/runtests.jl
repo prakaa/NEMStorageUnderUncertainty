05bde82fa0a6382c76d845834a63bffe75ebdfd6
prakaa <abiprakash007@gmail.com>
2022-10-28 10:11:08 +1100

    add month and single interval tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ using HiGHS: HiGHS
 using NEMStorageUnderUncertainty: NEMStorageUnderUncertainty
 using Test
 
-function test_run_model(data_path::String)
+function test_run_model(data_path::String, start_time::DateTime, end_time::DateTime)
     bess = NEMStorageUnderUncertainty.BESS(;
         power_capacity=30.0,
         energy_capacity=30.0,
@@ -19,17 +19,31 @@ function test_run_model(data_path::String)
     actual_price_data_path = joinpath(data_path, "dispatch_price")
     actual = NEMStorageUnderUncertainty.get_all_actual_prices(actual_price_data_path)
     (times, prices) = NEMStorageUnderUncertainty.get_regional_times_and_prices(
-        actual,
-        "NSW1";
-        actual_data_window=(DateTime(2021, 12, 1, 0, 5, 0), DateTime(2022, 1, 1, 0, 0, 0)),
+        actual, "NSW1"; actual_data_window=(start_time, end_time)
     )
-
+    if length(times) > 1
+        τ = NEMStorageUnderUncertainty._get_times_frequency_in_hours(times)
+    else
+        τ = 5.0 / 12.0
+    end
     model = NEMStorageUnderUncertainty._run_model(
-        HiGHS.Optimizer, bess, times, prices, NEMStorageUnderUncertainty.StandardArbitrage()
+        HiGHS.Optimizer,
+        bess,
+        times,
+        prices,
+        τ,
+        NEMStorageUnderUncertainty.StandardArbitrage(),
     )
-    return JuMP.objective_value(model)
+    return model
 end
 
-@testset "NEMStorageUnderUncertainty.jl" begin
-    @test isapprox(test_run_model("test/test_data"), 171887, atol=0.5)
+@testset "Run Model Tests" begin
+    month_model = test_run_model(
+        "test/test_data", DateTime(2021, 12, 1, 0, 5, 0), DateTime(2022, 1, 1, 0, 0, 0)
+    )
+    @test isapprox(JuMP.objective_value(model), 171887, atol=0.5)
+    interval_model = test_run_model(
+        "test/test_data", DateTime(2021, 12, 1, 0, 5, 0), DateTime(2021, 12, 1, 0, 5, 0)
+    )
+    @test JuMP.num_constraints(interval_model; count_variable_in_set_constraints=false) == 3
 end
