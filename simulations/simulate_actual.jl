b1f240a47030123d46277354dfdb28005eca86cd
abiprakash007@gmail.com <abiprakash007@gmail.com>
2022-11-03 17:36:39 +1100

    refactor sim

--- simulations/simulate_actual.jl
+++ simulations/simulate_actual.jl
@@ -20,7 +20,7 @@ function set_optimizer(optimizer_str::String)
         )
     elseif optimizer_str == "HiGHS"
         optimizer = optimizer_with_attributes(
-            HiGHS.Optimizer, "mip_rel_gap" => mip_optim_gap
+            HiGHS.Optimizer, "mip_rel_gap" => mip_optim_gap, "threads" => 10
         )
     elseif optimizer_str == "Cbc"
         optimizer = optimizer_with_attributes(Cbc.Optimizer, "ratioGap" => mip_optim_gap)
@@ -30,22 +30,22 @@ function set_optimizer(optimizer_str::String)
     return optimizer
 end
 
+function collate_actual_data(region::String)
+    @info("Collating actual data")
+    all_actual_data = NEMStorageUnderUncertainty.get_all_actual_data("data/dispatch_price")
+    actual_data = NEMStorageUnderUncertainty.get_ActualData(all_actual_data, region)
+    return all_actual_data, actual_data
+end
+
 function simulate_actual(
-    data::NEMStorageUnderUncertainty.ActualData, binding::T, horizon::T
+    optimizer::OptimizerWithAttributes,
+    storage::NEMStorageUnderUncertainty.StorageDevice,
+    data::NEMStorageUnderUncertainty.ActualData,
+    binding::T,
+    horizon::T;
+    start_time::DateTime,
+    end_time::DateTime,
 ) where {T<:Period}
-    optimizer = set_optimizer("Gurobi")
-    storage = NEMStorageUnderUncertainty.BESS(;
-        power_capacity=30.0,
-        energy_capacity=30.0,
-        soc_min=0.1 * 30.0,
-        soc_max=0.9 * 30.0,
-        η_charge=0.95,
-        η_discharge=0.95,
-        soc₀=0.5 * 30.0,
-        throughput=0.0,
-    )
-    (start_time, end_time) = (DateTime(2021, 1, 1, 0, 0, 0), DateTime(2022, 1, 1, 0, 0, 0))
-    @info("Starting simulations")
     results = NEMStorageUnderUncertainty.simulate_storage_operation(
         optimizer,
         storage,
@@ -57,16 +57,34 @@ function simulate_actual(
         binding=binding,
         horizon=horizon,
         silent=true,
-        show_progress=false,
+        show_progress=true,
     )
     return results
 end
 
 function main()
-    @info("Collating actual data")
-    actual_data = NEMStorageUnderUncertainty.make_ActualData(
-        "data/dispatch_price", "NSW1", nothing
+    optimizer = set_optimizer("HiGHS")
+    storage = NEMStorageUnderUncertainty.BESS(;
+        power_capacity=30.0,
+        energy_capacity=30.0,
+        soc_min=0.1 * 30.0,
+        soc_max=0.9 * 30.0,
+        η_charge=0.95,
+        η_discharge=0.95,
+        soc₀=0.5 * 30.0,
+        throughput=0.0,
     )
+    (start_time, end_time) = (DateTime(2021, 1, 1, 0, 0, 0), DateTime(2022, 1, 1, 0, 0, 0))
+    all_actual_data, actual_data = collate_actual_data("NSW1")
+    @info("Simulating perfect foresight")
+    perfect_foresight_result = NEMStorageUnderUncertainty.run_perfect_foresight(
+        optimizer,
+        storage,
+        actual_data,
+        NEMStorageUnderUncertainty.StandardArbitrage();
+        silent=false,
+    )
+    @info("Finished simulating perfect foresight")
     lookaheads = [
         Minute(5),
         Minute(15),
@@ -78,17 +96,29 @@ function main()
     ]
     all_results = DataFrame[]
     Threads.@threads for horizon in lookaheads
-        @info("Starting lookahead $horizon")
-        results = simulate_actual(actual_data, Minute(5), horizon)
+        @info("Starting lookahead $horizon simulation")
+        results = simulate_actual(
+            optimizer,
+            storage,
+            actual_data,
+            Minute(5),
+            horizon;
+            start_time=start_time,
+            end_time=end_time,
+        )
         push!(all_results, results)
-        @info("Completed lookahead $horizon")
+        @info("Completed lookahead $horizon simulation")
     end
-    return all_results
+    df = vcat(all_results..., perfect_foresight_result)
+    if !isdir(joinpath(@__DIR__, "results"))
+        mkdir(joinpath(@__DIR__, "results"))
+    end
+    CSV.write(
+        joinpath(@__DIR__, "results", "actual_StandardArb_NoDeg_2021_lookaheads.csv"), df
+    )
+    df = NEMStorageUnderUncertainty.calculate_actual_revenue!(df, all_actual_data)
+
+    return df
 end
 
-all_results = main()
-df = vcat(all_results...)
-if !isdir(joinpath(@__DIR__, "results"))
-    mkdir(joinpath(@__DIR__, "results"))
-end
-CSV.write(joinpath(@__DIR__, "results", "actual_StandardArb_NoDeg_2021_lookaheads.csv"), df)
+sim_results = main()
