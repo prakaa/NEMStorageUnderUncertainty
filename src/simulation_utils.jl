341bd2e643f24b6a6bf76b2ecf08d2caf30398c5
abiprakash007@gmail.com <abiprakash007@gmail.com>
2022-11-03 14:55:00 +1100

    remove useless util, add actual revenue calc

--- src/simulation_utils.jl
+++ src/simulation_utils.jl
@@ -1,7 +1,22 @@
-function make_ActualData(
-    data_path::String, region::String, time_window::Union{Tuple{DateTime,DateTime},Nothing}
-)
-    all_actual_data = NEMStorageUnderUncertainty.get_all_actual_data(data_path)
-    actual = NEMStorageUnderUncertainty.get_ActualData(all_actual_data, region, time_window)
-    return actual
+function calculate_actual_revenue!(sim_results::DataFrame, actual_price_data::DataFrame)
+    binding_sim_results = filter(:status => status -> status == "binding", sim_results)
+    sort!(binding_sim_results, :simulated_time)
+    sort!(actual_price_data, :SETTLEMENTDATE)
+    @assert(
+        actual_price_data.SETTLEMENTDATE[1] ≥ binding_sim_results.simulated_time[1],
+        "Actual price data starts after the first simulated datetime"
+    )
+    @assert(
+        actual_price_data.SETTLEMENTDATE[end] ≤ binding_sim_results.simulated_time[end],
+        "Actual price data ends before the last simulated datetime"
+    )
+    actual_price_data = rename(
+        actual_price_data, :SETTLEMENTDATE => :simulated_time, :RRP => :actual_price
+    )
+    merged = leftjoin(
+        binding_sim_results, actual_price_data; on=[:simulated_time, :REGIONID]
+    )
+    merged[!, :revenue] =
+        merged[!, :actual_price_data] .* (merged[!, :discharge_mw] .- merged[!, :charge_mw])
+    return merged
 end
