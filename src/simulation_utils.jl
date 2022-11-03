d76e45e70c313d884568bdb7a52e03b15a0c662b
abiprakash007@gmail.com <abiprakash007@gmail.com>
2022-11-03 17:32:36 +1100

    fix asserts and calc in actual revenue calc and add run perfect foresight

--- src/simulation_utils.jl
+++ src/simulation_utils.jl
@@ -1,13 +1,15 @@
-function calculate_actual_revenue!(sim_results::DataFrame, actual_price_data::DataFrame)
+function calculate_actual_revenue!(
+    sim_results::DataFrame, actual_price_data::DataFrame, τ::Float64
+)
     binding_sim_results = filter(:status => status -> status == "binding", sim_results)
     sort!(binding_sim_results, :simulated_time)
     sort!(actual_price_data, :SETTLEMENTDATE)
     @assert(
-        actual_price_data.SETTLEMENTDATE[1] ≥ binding_sim_results.simulated_time[1],
+        actual_price_data.SETTLEMENTDATE[1] ≤ binding_sim_results.simulated_time[1],
         "Actual price data starts after the first simulated datetime"
     )
     @assert(
-        actual_price_data.SETTLEMENTDATE[end] ≤ binding_sim_results.simulated_time[end],
+        actual_price_data.SETTLEMENTDATE[end] ≥ binding_sim_results.simulated_time[end],
         "Actual price data ends before the last simulated datetime"
     )
     actual_price_data = rename(
@@ -17,6 +19,32 @@ function calculate_actual_revenue!(sim_results::DataFrame, actual_price_data::Da
         binding_sim_results, actual_price_data; on=[:simulated_time, :REGIONID]
     )
     merged[!, :revenue] =
-        merged[!, :actual_price_data] .* (merged[!, :discharge_mw] .- merged[!, :charge_mw])
+        merged[!, :actual_price] .* (merged[!, :discharge_mw] .- merged[!, :charge_mw]) * τ
     return merged
+end
+
+function run_perfect_foresight(
+    optimizer::OptimizerWithAttributes,
+    storage::StorageDevice,
+    actual_data::ActualData,
+    formulation::StorageModelFormulation;
+    silent::Bool=false,
+    time_limit_sec::Union{Float64,Nothing}=nothing,
+    string_names::Bool=true,
+)
+    model = run_model(
+        optimizer,
+        storage,
+        actual_data.prices,
+        actual_data.times,
+        actual_data.τ,
+        formulation;
+        silent=silent,
+        time_limit_sec=time_limit_sec,
+        string_names=string_names,
+    )
+    results = _retrieve_results(
+        model, actual_data.times[1], actual_data.times[2], actual_data.times[end]
+    )
+    return results
 end
