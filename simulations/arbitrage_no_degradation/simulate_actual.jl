2a5b53b1869833dfa5196d0fc352a9bc12b366d1
abiprakash007@gmail.com <abiprakash007@gmail.com>
2022-11-14 12:37:11 +1100

    remove Gurobi and Cbc options and refactor to allow CLI args

--- simulations/arbitrage_no_degradation/simulate_actual.jl
+++ simulations/arbitrage_no_degradation/simulate_actual.jl
@@ -1,35 +1,17 @@
 import MathOptInterface: OptimizerWithAttributes
-using Cbc
 using CSV
 using Dates
 using DataFrames
-using Gurobi
 using HiGHS
 using JuMP
 using NEMStorageUnderUncertainty: NEMStorageUnderUncertainty
 using ProgressMeter
 
-# suppresses Gurobi solver output
-const GUROBI_ENV = Gurobi.Env()
-
-function set_optimizer(optimizer_str::String)
+function set_optimizer()
     mip_optim_gap = 0.005
-    if optimizer_str == "Gurobi"
-        # Suppresses Gurobi solver output
-        optimizer = optimizer_with_attributes(
-            () -> Gurobi.Optimizer(GUROBI_ENV),
-            "MIPGap" => mip_optim_gap,
-            "NodefileStart" => 0.5,
-        )
-    elseif optimizer_str == "HiGHS"
-        optimizer = optimizer_with_attributes(
-            HiGHS.Optimizer, "mip_rel_gap" => mip_optim_gap, "threads" => 20
-        )
-    elseif optimizer_str == "Cbc"
-        optimizer = optimizer_with_attributes(Cbc.Optimizer, "ratioGap" => mip_optim_gap)
-    else
-        error("Specify valid optimizer")
-    end
+    optimizer = optimizer_with_attributes(
+        HiGHS.Optimizer, "mip_rel_gap" => mip_optim_gap, "threads" => 20
+    )
     return optimizer
 end
 
@@ -50,7 +32,7 @@ function simulate(
     start_time::DateTime,
     end_time::DateTime,
 ) where {T<:Period}
-    optimizer = set_optimizer("HiGHS")
+    optimizer = set_optimizer()
     results = NEMStorageUnderUncertainty.simulate_storage_operation(
         optimizer,
         storage,
@@ -69,11 +51,11 @@ function simulate(
     return results
 end
 
-function simulate_actual2021_StandardArb_NoDeg_lookaheads()
+function simulate_actual2021_StandardArb_NoDeg_lookaheads(power::Float64, energy::Float64)
     if !isdir(joinpath(@__DIR__, "results"))
         mkdir(joinpath(@__DIR__, "results"))
     end
-    optimizer = set_optimizer("HiGHS")
+    optimizer = set_optimizer()
     lookaheads = [
         Minute(5),
         Minute(15),
@@ -87,62 +69,57 @@ function simulate_actual2021_StandardArb_NoDeg_lookaheads()
     (start_time, end_time) = (DateTime(2021, 1, 1, 0, 0, 0), DateTime(2022, 1, 1, 0, 0, 0))
     (data_start, data_end) = (start_time, end_time + lookaheads[end])
     all_actual_data, actual_data = collate_actual_data("NSW1", data_start, data_end)
-    c_multipliers = (0.25, 0.5, 1.0, 2.0, 4.0)
-    for c_multiplier in c_multipliers
-        energy = 100.0
-        power = energy * c_multiplier
-        storage = NEMStorageUnderUncertainty.BESS(;
-            power_capacity=power,
-            energy_capacity=energy,
-            soc_min=0.1 * energy,
-            soc_max=0.9 * energy,
-            η_charge=0.95,
-            η_discharge=0.95,
-            soc₀=0.5 * energy,
-            throughput=0.0,
-        )
-        @info("BESS $power MW $energy MWh")
-        @info("Simulating perfect foresight")
-        perfect_foresight_result = NEMStorageUnderUncertainty.run_perfect_foresight(
-            optimizer,
+    storage = NEMStorageUnderUncertainty.BESS(;
+        power_capacity=power,
+        energy_capacity=energy,
+        soc_min=0.1 * energy,
+        soc_max=0.9 * energy,
+        η_charge=0.95,
+        η_discharge=0.95,
+        soc₀=0.5 * energy,
+        throughput=0.0,
+    )
+    @info("BESS $power MW $energy MWh")
+    @info("Simulating perfect foresight")
+    perfect_foresight_result = NEMStorageUnderUncertainty.run_perfect_foresight(
+        optimizer,
+        storage,
+        actual_data,
+        NEMStorageUnderUncertainty.StandardArbitrage(),
+        NEMStorageUnderUncertainty.NoDegradation();
+        silent=true,
+    )
+    @info("Finished simulating perfect foresight")
+    all_results = DataFrame[]
+    p = Progress(length(lookaheads))
+    for horizon in lookaheads
+        @info("Starting lookahead $horizon simulation")
+        results = simulate(
             storage,
             actual_data,
-            NEMStorageUnderUncertainty.StandardArbitrage(),
-            NEMStorageUnderUncertainty.NoDegradation();
-            silent=true,
+            Minute(5),
+            horizon;
+            start_time=start_time,
+            end_time=end_time,
         )
-        @info("Finished simulating perfect foresight")
-        all_results = DataFrame[]
-        p = Progress(length(lookaheads))
-        Threads.@threads for horizon in lookaheads
-            @info("Starting lookahead $horizon simulation")
-            results = simulate(
-                storage,
-                actual_data,
-                Minute(5),
-                horizon;
-                start_time=start_time,
-                end_time=end_time,
-            )
-            push!(all_results, results)
-            @info("Completed lookahead $horizon simulation")
-            next!(p)
-        end
-        df = vcat(all_results..., perfect_foresight_result)
-        df = NEMStorageUnderUncertainty.calculate_actual_revenue!(
-            df, all_actual_data, actual_data.τ
-        )
-        NEMStorageUnderUncertainty.results_to_jld2(
-            joinpath(
-                @__DIR__,
-                "results",
-                "NSW_$(energy)MWh_StandardArb_NoDeg_2021_lookaheads.jld2",
-            ),
-            "actual",
-            "$(power)MW",
-            df,
-        )
+        push!(all_results, results)
+        @info("Completed lookahead $horizon simulation")
+        next!(p)
     end
+    df = vcat(all_results..., perfect_foresight_result)
+    df = NEMStorageUnderUncertainty.calculate_actual_revenue!(
+        df, all_actual_data, actual_data.τ
+    )
+    return NEMStorageUnderUncertainty.results_to_jld2(
+        joinpath(
+            @__DIR__, "results", "NSW_$(energy)MWh_StandardArb_NoDeg_2021_lookaheads.jld2"
+        ),
+        "actual",
+        "$(power)MW",
+        df,
+    )
 end
 
-simulate_actual2021_StandardArb_NoDeg_lookaheads()
+@assert !isempty(ARGS) "Provide power and energy capacity as arguments (in that order)"
+power_and_energy = parse.(Float64, ARGS)
+simulate_actual2021_StandardArb_NoDeg_lookaheads(power_and_energy[1], power_and_energy[2])
