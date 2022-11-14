1255f83efc29d7de2c8070f9b1d78feb54be9a47
abiprakash007@gmail.com <abiprakash007@gmail.com>
2022-11-14 13:32:15 +1100

    pass permutations via shell scripts for queueing pbs jobs

--- queue_arbthroughputlimited_nodeg.sh
+++ queue_arbthroughputlimited_nodeg.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
+for energy in 100
+do
+for powerratio in 0.125 0.25 0.5 1 2 4 8
+do
+power=$(echo "$energy*$powerratio" | bc -l)
 for file in pbs/arbthroughputlimited_nodeg/*
 do
-qsub $file
-sleep 2m
+qsub -F "p=$power e=$energy" $file
+sleep 1m
+done
+done
 done
