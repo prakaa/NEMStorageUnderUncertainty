9139d8b564945b7bb638f197731a785f5a4e04dc
abiprakash007@gmail.com <abiprakash007@gmail.com>
2022-11-16 13:46:31 +1100

    initial changes to plotting

--- plotting/plot_revenues_across_lookaheads.jl
+++ plotting/plot_revenues_across_lookaheads.jl
@@ -14,10 +14,7 @@ function _sort_bess_capacities(data::DataFrame)
     return data
 end
 
-function plot_revenues_across_simulations(
-    jld2_path::String, title::String; percentage_of_perfect_foresight=false
-)
-    data = load(jld2_path)
+function _get_revenue_summary_data(data::Dict{String,Any})
     revenues = []
     bess_type = []
     lookaheads = []
@@ -40,7 +37,15 @@ function plot_revenues_across_simulations(
     )
     summary_data = _sort_bess_capacities(summary_data)
     summary_data = unstack(summary_data, :sim, :lookahead, :revenue)
-    plot_data = rename(summary_data, Symbol("526500") => Symbol("Perfect Foresight"))
+    summary_data = rename(summary_data, Symbol("526500") => Symbol("Perfect Foresight"))
+    return summary_data
+end
+
+function plot_revenues_across_simulations(
+    jld2_path::String, title::String; percentage_of_perfect_foresight=false
+)
+    data = load(jld2_path)
+    plot_data = _get_revenue_summary_data(data)
     if percentage_of_perfect_foresight
         for col in propertynames(plot_data[:, 2:end])
             plot_data[:, col] = @. plot_data[:, col] /
@@ -55,24 +60,84 @@ function plot_revenues_across_simulations(
     end
     plot_matrix = Float64.(Array(plot_data[:, 2:end]))
     groupby_labels = permutedims(names(plot_data)[2:end])
-    colors = [[color] for color in palette(:roma, 8)]'
+    colors = [[color] for color in palette(:roma, length(groupby_labels))]'
+    fnt = "Source Sans Pro"
     return groupedbar(
         plot_data[:, :sim],
         plot_matrix;
-        rot=45,
+        xrot=45,
         ylabel=ylabel,
-        size=(800, 650),
-        fontfamily="serif",
-        legend=Symbol(:outer, :right),
-        framestyle=:box,
+        size=(800, 600),
+        lw=0,
         title=title,
         label=groupby_labels,
         color=colors,
         bg="#f0f0f0",
         legend_title="Lookahead\n(minutes)",
-        bottom_margin=50px,
+        margin=5px,
         yscale=scale,
+        guidefont=font(fnt, 12),
+        titlefont=font(fnt, 16),
+        tickfont=font(fnt, 10),
+        legendtitlefont=font(fnt, 8),
+        legendfont=font(fnt, 8),
+        extra_kwargs=KW(
+            :plot => KW(
+                :legend =>
+                    KW(:x => 0.5, :y => -0.01, :orientation => "h", :borderwidth => 0),
+            ),
+        ),
     )
+end
+
+function plot_value_of_information_and_foresight(jld2_path::String, title::String)
+    data = load(jld2_path)
+    plot_data = _get_revenue_summary_data(data)
+    actual_caps = [
+        cap[2] for cap in split.(unique(plot_data.sim), "/") if cap[1] == "actual"
+    ]
+    forecast_caps = [
+        cap[2] for cap in split.(unique(plot_data.sim), "/") if cap[1] == "forecast"
+    ]
+    caps = intersect(actual_caps, forecast_caps)
+    v_pi = DataFrame[]
+    v_pf = DataFrame[]
+    for cap in caps
+        value_of_pi =
+            plot_data[plot_data.sim .== "actual/$cap", 2:(end - 1)] .-
+            plot_data[plot_data.sim .== "forecast/$cap", 2:(end - 1)]
+        push!(v_pi, hcat(DataFrame(:bess_mw => cap), value_of_pi))
+        value_of_pf =
+            plot_data[plot_data.sim .== "forecast/$cap", end] .-
+            plot_data[plot_data.sim .== "forecast/$cap", 2:(end - 1)]
+        push!(v_pf, hcat(DataFrame(:bess_mw => cap), value_of_pf))
+    end
+    v_pi = vcat(v_pi...)
+    v_pf = vcat(v_pf...)
+
+    scale = :log10
+    ylabel = "Revenue (AUD)"
+    return plot_data
+    #plot_matrix = Float64.(Array(plot_data[:, 2:end]))
+    #groupby_labels = permutedims(names(plot_data)[2:end])
+    #colors = [[color] for color in palette(:roma, 8)]'
+    #return groupedbar(
+    #    plot_data[:, :sim],
+    #    plot_matrix;
+    #    rot=45,
+    #    ylabel=ylabel,
+    #    size=(800, 650),
+    #    fontfamily="serif",
+    #    legend=Symbol(:outer, :right),
+    #    framestyle=:box,
+    #    title=title,
+    #    label=groupby_labels,
+    #    color=colors,
+    #    bg="#f0f0f0",
+    #    legend_title="Lookahead\n(minutes)",
+    #    bottom_margin=50px,
+    #    yscale=scale,
+    #)
 end
 
 function plot_standardarb_nodeg()
@@ -134,5 +199,6 @@ function plot_standardarb_throughput_limits()
     )
 end
 
+plotlyjs()
 plot_standardarb_nodeg()
 plot_standardarb_throughput_limits()
