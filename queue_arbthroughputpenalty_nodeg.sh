9f573670c0b9eb552f52b7036522cfea90e36bd3
abiprakash007@gmail.com <abiprakash007@gmail.com>
2022-11-18 16:36:19 +1100

    add queueing shell script for throughpout penalty

---
+++ queue_arbthroughputpenalty_nodeg.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+for energy in 100
+do
+for cost in 500.0 1000.0 1500.0 2000.0
+do
+for powerratio in 0.125 0.25 0.5 1 2 4 8
+do
+power=$(echo "$energy*$powerratio" | bc -l)
+for file in pbs/arbitrage_throughputpenalty_no_deg/*
+do
+qsub -v "power=$power,energy=$energy,cost=$cost" $file
+sleep 30s
+done
+done
+done
+done
