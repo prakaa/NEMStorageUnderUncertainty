6720e36c1e26a5e9d89829d2b1a19e4373f094a9
abiprakash007@gmail.com <abiprakash007@gmail.com>
2022-11-21 10:29:52 +1100

    fix penalty costs submitted for simulation

--- queue_arbthroughputpenalty_nodeg.sh
+++ queue_arbthroughputpenalty_nodeg.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
+
+# costs roughly reflect a range of build costs for BESS of varuious storage duration as outlined in AEMO I&A 2021 Workbook
+# for 2020-2021, costs in $/kW are ~800 $/kW (1 hr storage) and ~3000 $/kW (8 hour storage)
+# translating to energy capacity, 800,000 $/MWh and 375,000 $/MWh
+
 for energy in 100
 do
-for cost in 500.0 1000.0 1500.0 2000.0
+for cost in 200000.0 400000.0 600000.0 800000.0
 do
 for powerratio in 0.125 0.25 0.5 1 2 4 8
 do
