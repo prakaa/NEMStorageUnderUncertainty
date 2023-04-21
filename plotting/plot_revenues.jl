952bf5b85fb594d934f504f29c9cfca0d62805bc
abiprakash007@gmail.com <abiprakash007@gmail.com>
2023-04-21 10:19:08 +1000

    plot all formulation revenues - quick and fast code

--- plotting/plot_revenues.jl
+++ plotting/plot_revenues.jl
@@ -3,6 +3,117 @@ using NEMStorageUnderUncertainty
 using CairoMakie
 using JLD2
 
+function _makie_plot_all(
+    plot_data::Dict{String,Any},
+    title::String,
+    ylabel::String,
+    yscale::Function,
+    fillto::Float64,
+)
+    formulation_label_map = Dict(
+        "arbitrage_no_degradation" => "Arb",
+        "arbitrage_throughputpenalty_no_degradation" => "TP Penalty",
+        "arbitrage_throughputlimited_no_degradation" => "TP Limited",
+        "arbitrage_capcontracted_no_degradation" => "Cap + TP Pen.",
+        "arbitrage_discounted_no_degradation" => "Discounting + TP Pen. (600000)",
+    )
+    actual_rev = DataFrame[]
+    forecast_rev = DataFrame[]
+    for (key, value) in pairs(plot_data)
+        hundred_mw_data = value[value.power_capacity .== 100.0, :]
+        hundred_mw_data = hundred_mw_data[
+            (hundred_mw_data.lookahead .!= "Perfect Foresight") .& (hundred_mw_data.lookahead .!= "5"),
+            All(),
+        ]
+        if contains(key, "/")
+            (formulation, param) = string.(split(key, "/"))
+            hundred_mw_data[:, :param] .= fill(
+                uppercasefirst(param), size(hundred_mw_data)[1]
+            )
+        else
+            (formulation, param) = (key, nothing)
+            hundred_mw_data[:, :param] .= fill("", size(hundred_mw_data)[1])
+        end
+        push!(actual_rev, hundred_mw_data[hundred_mw_data.data_type .== "actual", :])
+        push!(forecast_rev, hundred_mw_data[hundred_mw_data.data_type .== "forecast", :])
+    end
+    actual_rev = vcat(actual_rev...)
+    forecast_rev = vcat(forecast_rev...)
+    non_param_formulations = unique(actual_rev[actual_rev.param .== "", :formulation])
+    param_formulations = unique(actual_rev[actual_rev.param .!= "", :formulation])
+    for df in (actual_rev, forecast_rev)
+        df.formulation = map(x -> formulation_label_map[x], df.formulation)
+        df.label = fill("", size(df)[1])
+        df[df.param .!= "", :label] .=
+            df[df.param .!= "", :formulation] .* " [" .* df[df.param .!= "", :param] .* "]"
+        df[df.param .== "", :label] .= df[df.param .== "", :formulation]
+    end
+    (sims, lookaheads) = (sort(unique(actual_rev.label)), unique(actual_rev.lookahead))
+    colors = []
+    seq_colormaps = (:Hokusai2, :Tam)
+    append!(colors, Makie.wong_colors()[3:(2 + length(non_param_formulations))])
+    for (i, f) in enumerate(param_formulations)
+        append!(
+            colors,
+            [
+                c for c in cgrad(
+                    seq_colormaps[i],
+                    length([
+                        sim for sim in sims if contains(sim, formulation_label_map[f])
+                    ]);
+                    categorical=true,
+                    alpha=0.8,
+                )
+            ],
+        )
+    end
+    fig = Figure(; backgroundcolor="#f0f0f0", resolution=(800, 600))
+    for (stroke_col, bar_colors, df) in
+        zip((:gray, :black), (colors, :transparent), (actual_rev, forecast_rev))
+        sort!(df, [:formulation, :param, :lookahead])
+        xs = [findfirst(x -> x == lookahead, lookaheads) for lookahead in df.lookahead]
+        groups = [findfirst(x -> x == sim, sims) for sim in df.label]
+        ax = Axis(
+            fig[1, 1];
+            xticks=(1:length(lookaheads), lookaheads),
+            title,
+            ylabel=ylabel,
+            yscale=yscale,
+        )
+        if typeof(bar_colors) != Symbol
+            bar_colors = bar_colors[groups]
+        end
+        barplot!(
+            ax,
+            xs,
+            df.revenue;
+            dodge=groups,
+            color=bar_colors,
+            strokecolor=stroke_col,
+            strokewidth=1,
+            fillto=fillto,
+        )
+        ylims!(ax, 1.0, 20000000.0)
+    end
+    Legend
+    elements = [PolyElement(; polycolor=colors[i]) for i in 1:length(sims)]
+    actual_forecast = [
+        PolyElement(; polycolor=:transparent, strokecolor=c, strokewidth=1) for
+        c in (:gray, :black)
+    ]
+    Legend(
+        fig[1, 2],
+        [actual_forecast, elements],
+        [["Actual", "Forecast"], sims],
+        ["Data type", "Simulated formulation"];
+        framevisible=false,
+        patchcolor="#f0f0f0",
+    )
+    energy = unique(actual_rev.energy_capacity)[]
+    power = unique(actual_rev.power_capacity)[]
+    return fig, energy, power
+end
+
 function _makie_plot(
     plot_data::DataFrame, title::String, ylabel::String, yscale::Function, fillto::Float64
 )
@@ -99,6 +210,23 @@ function plot_revenues_across_formulations(
     end
 end
 
+function plot_revenues_all_formulations(data_path::String, save_path::String)
+    data_files = [f for f in readdir(data_path) if contains(f, "summary_results")]
+    for file in data_files
+        state = string(match(r"([A-Z]{2,3})_.*", file).captures[])
+        data = load(joinpath(data_path, file))
+        ylabel = "Revenue (AUD)"
+        yscale = identity
+        fillto = 0.0
+        title = ""
+        (fig, energy, power) = _makie_plot_all(data, title, ylabel, yscale, fillto)
+        title = "$power MW/$energy MWh BESS - Revenues - $state Prices, 2021"
+        Label(fig[0, :]; text=title, fontsize=22, font="Source Sans Pro")
+        filename = "$(state)_$(energy)MWh_$(power)MW_allformulations_revenue.pdf"
+        save(joinpath(save_path, filename), fig; pt_per_unit=1)
+    end
+end
+
 data_path = joinpath("results", "data")
 plot_path = joinpath("results", "plots", "revenues")
 if !ispath(plot_path)
@@ -108,6 +236,7 @@ end
 @assert ispath(data_path) "Results data not compiled. Run 'make compile_results'."
 
 NEMStorageUnderUncertainty.set_project_plot_theme!()
+plot_revenues_all_formulations(joinpath("results", "data"), plot_path)
 for percentage_of_perfect_foresight in (false, true)
     plot_revenues_across_formulations(
         joinpath("results", "data"), percentage_of_perfect_foresight, plot_path
