1eb63b79fadcf6468de8de32c9f154f6335043ab
prakaa <abiprakash007@gmail.com>
2024-07-31 16:17:56 +1000

    fix charge calc, add all cols and write to csv

--- results/standard_vs_apb_results.jl
+++ results/standard_vs_apb_results.jl
@@ -2,6 +2,7 @@ using DataFrames
 using JLD2
 using NEMStorageUnderUncertainty
 using Statistics
+using CSV
 
 const SCENARIO_COLS = [
     :formulation, :energy_capacity, :power_capacity, :data_type, :lookahead
@@ -16,6 +17,8 @@ function merge_summary_data(standard_data_path::String, apb_data_path::String)
     all_merged = DataFrame[]
     for key in keys(standard_data)
         (df_standard, df_apb) = (standard_data[key], apb_data[key])
+        df_standard.formulation = fill(key, size(df_standard, 1))
+        df_apb.formulation = fill(key, size(df_apb, 1))
         df_standard_forecast = df_standard[df_standard.data_type.=="forecast", :]
         df_apb_forecast = df_apb[df_apb.data_type.=="forecast", :]
         merged = innerjoin(
@@ -32,22 +35,32 @@ end
 """
 Calculates three metrics
 
+- `neg_rev_discharge_percent_standard`: Negative revenue from discharge in standard scenario
+    as % of standard revenue
+- `neg_rev_charge_500_percent_standard`: Negative revenue from charging in standard scenario
+    when price ≥ 500 as % of standard revenue
+- `neg_rev_discharge_percent_apb`: Negative revenue from discharge in APB scenario
+    as % of APB revenue
+- `neg_rev_charge_500_percent_apb`: Negative revenue from charging in APB scenario
+    when price ≥ 500 as % of APB revenue
 - `rev_improvement`: Improvement in actual-prices-binding scenario
     from standard scenario as % of standard revenue
-- `neg_rev_discharge_percent`: Negative revenue from discharge in standard scenario
-    as % of standard revenue
-- `neg_rev_charge_500_percent`: Negative revenue from charging in standard scenario
-    when price ≥ 500 as % of standard revenue
 """
 function calculate_metrics(all_merged_data::Array{DataFrame})
     all_metrics = DataFrame[]
     metric_cols = [
-        :rev_improvement_percent, :neg_rev_discharge_percent, :neg_rev_charge_500_percent
+        :rev_improvement,
+        :neg_rev_discharge_standard,
+        :neg_rev_charge_500_standard,
+        :neg_rev_discharge_apb,
+        :neg_rev_charge_500_apb,
     ]
     for df in all_merged_data
-        df[:, metric_cols[1]] = @. (df[:, :revenue_apb] - df[:, :revenue_standard]) / (df[:, :revenue_standard]) * 100
-        df[:, metric_cols[2]] = @. -1 * df[:, :neg_rev_discharge_standard] / (df[:, :revenue_standard]) * 100
-        df[:, metric_cols[3]] = @. -1 * df[:, :neg_rev_discharge_standard] / (df[:, :revenue_standard]) * 100
+        df[:, metric_cols[1]] = @. (df[:, :revenue_apb] - df[:, :revenue_standard]) / (df[:, :revenue_standard])
+        df[:, metric_cols[2]] = @. -1 * df[:, :neg_rev_discharge_standard] / (df[:, :revenue_standard])
+        df[:, metric_cols[3]] = @. -1 * df[:, :neg_rev_charge_500_standard] / (df[:, :revenue_standard])
+        df[:, metric_cols[4]] = @. -1 * df[:, :neg_rev_discharge_apb] / (df[:, :revenue_apb])
+        df[:, metric_cols[5]] = @. -1 * df[:, :neg_rev_charge_500_apb] / (df[:, :revenue_apb])
         push!(all_metrics, df[:, cat(SCENARIO_COLS, metric_cols, dims=1)])
     end
     return all_metrics
@@ -62,27 +75,34 @@ function summarise_metrics(all_metrics::Array{DataFrame})
     min_rev_improvements = Float64[]
     med_rev_improvements = Float64[]
     max_rev_improvements = Float64[]
-    max_neg_rev_discharge_percents = Float64[]
-    max_neg_rev_charge_500_percents = Float64[]
+    max_neg_rev_discharge_standard = Float64[]
+    max_neg_rev_charge_500_standard = Float64[]
+    max_neg_rev_discharge_apb = Float64[]
+    max_neg_rev_charge_500_apb = Float64[]
     for df in all_metrics
         filtered_df = _filter_results!(df)
         push!(formulations, only(unique(filtered_df.formulation)))
-        push!(min_rev_improvements, minimum(filtered_df[filtered_df.lookahead.!="5", :rev_improvement_percent]))
-        push!(med_rev_improvements, median(filtered_df.rev_improvement_percent))
-        push!(max_rev_improvements, maximum(filtered_df.rev_improvement_percent))
-        push!(max_neg_rev_discharge_percents, maximum(filtered_df.neg_rev_discharge_percent))
-        push!(max_neg_rev_charge_500_percents, maximum(filtered_df.neg_rev_charge_500_percent))
+        push!(min_rev_improvements, minimum(filtered_df[filtered_df.lookahead.!="5", :rev_improvement]))
+        push!(med_rev_improvements, median(filtered_df.rev_improvement))
+        push!(max_rev_improvements, maximum(filtered_df.rev_improvement))
+        push!(max_neg_rev_discharge_standard, maximum(filtered_df.neg_rev_discharge_standard))
+        push!(max_neg_rev_charge_500_standard, maximum(filtered_df.neg_rev_charge_500_standard))
+        push!(max_neg_rev_discharge_apb, maximum(filtered_df.neg_rev_discharge_apb))
+        push!(max_neg_rev_charge_500_apb, maximum(filtered_df.neg_rev_charge_500_apb))
     end
     return DataFrame(
         :formulation => formulations,
         :min_rev_improvements_lkgt5 => min_rev_improvements,
         :med_rev_improvements => med_rev_improvements,
         :max_rev_improvement => max_rev_improvements,
-        :max_neg_rev_discharge_percent => max_neg_rev_discharge_percents,
-        :max_neg_rev_charge_500_percent => max_neg_rev_charge_500_percents
+        :max_neg_rev_discharge_standard => max_neg_rev_discharge_standard,
+        :max_neg_rev_charge_500_standard => max_neg_rev_charge_500_standard,
+        :max_neg_rev_discharge_apb => max_neg_rev_discharge_apb,
+        :max_neg_rev_charge_500_apb => max_neg_rev_charge_500_apb,
     )
 end
 
 merged = merge_summary_data("results/data/NSW_summary_results.jld2", "results/data/actual-prices-binding/NSW_summary_results_apb.jld2")
 metrics = calculate_metrics(merged)
 summary = summarise_metrics(metrics)
+CSV.write("results/data/actual-prices-binding/rev_and_negrev_standard_vs_apb.csv", summary)
